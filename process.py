8678b3cb3fa56bad401c0e9f7915ee2384f3e207
gmendonca <gustavogmend@gmail.com>
2017-01-20 22:02:22 -0200

    More processing and adding pandas requirement

--- process.py
+++ process.py
@@ -9,11 +9,44 @@ table = soup.find("table")
 
 rows = table.findAll('tr')
 
+concursos = []
+datas = []
+numeros_1 = []
+numeros_2 = []
+numeros_3 = []
+numeros_4 = []
+numeros_5 = []
+numeros_6 = []
+list_ganhadores_sena = []
+list_ganhadores_quina = []
+list_ganhadores_quadra = []
+
 for tr in rows:
     cols = tr.findAll('td')
     if len(cols) > 3:
         concurso, data, numero_1, numero_2, numero_3, numero_4, numero_5, numero_6, arrecadacao, ganhadores_sena, \
         cidade, uf, rateio_sena, ganhadores_quina, rateio_quina, ganhadores_quadra, rateio_quadra, acumulado, \
         valor_acomulado, estimativa, acomulado_mega_virada = [c.text for c in cols]
-        print concurso, data, numero_1, numero_2, numero_3, numero_4, numero_5, numero_6, \
-            ganhadores_sena, ganhadores_quina, ganhadores_quadra
+        concursos += [int(concurso)]
+        datas += [str(data)]
+        numeros_1 += [int(numero_1)]
+        numeros_2 += [int(numero_2)]
+        numeros_3 += [int(numero_3)]
+        numeros_4 += [int(numero_4)]
+        numeros_5 += [int(numero_5)]
+        numeros_6 += [int(numero_6)]
+        list_ganhadores_sena += [int(ganhadores_sena)]
+        list_ganhadores_quina += [int(ganhadores_quina)]
+        list_ganhadores_quadra += [int(ganhadores_quadra)]
+
+mega_sena = {'concursos': concursos,
+             'datas': datas,
+             'numeros_1': numeros_1,
+             'numeros_2': numeros_2,
+             'numeros_3': numeros_3,
+             'numeros_4': numeros_4,
+             'numeros_5': numeros_5,
+             'numeros_6': numeros_6,
+             'list_ganhadores_sena': list_ganhadores_sena,
+             'list_ganhadores_quina': list_ganhadores_quina,
+             'list_ganhadores_quadra': list_ganhadores_quadra}
